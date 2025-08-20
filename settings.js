1c428a6d67a6128979b00c665889a1a2fbb16edb
EazzyHacks <yhair7200@gmail.com>
2025-08-20 21:43:30 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -75,7 +75,7 @@ global.correo = 'yallico2024@gmail.com'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.catalogo = fs.readFileSync('./src/catalogo.jpg');
+global.nagi = fs.readFileSync('./src/nagi.jpg');
 global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
 global.ch = {
 ch1: '120363419947391620@newsletter',
