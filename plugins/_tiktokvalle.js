71fe763e87a3f65f237dff87c267d9017f6c39aa
EazzyHacks <yhair7200@gmail.com>
2025-08-21 08:47:31 +0100

    Add files via upload

---
+++ plugins/_tiktokvalle.js
@@ -0,0 +1,34 @@
+
+let handler  = async (m, { conn, usedPrefix, command }) => {
+let res = await tiktokvalle[Math.floor(Math.random() * tiktokvalle.length)]
+await m.react('🗣')
+conn.sendMessage(m.chat, { video: { url: res }, caption: `» 𝗩𝗔𝗟𝗟𝗘𝗡𝗔𝗧𝗢𝗦 𝗧𝗜𝗞𝗧𝗢𝗞 💔` }, { quoted: m })}
+handler.help = ['tiktokhot']
+handler.tags = ['random']
+handler.command = /^(tiktokhot)$/i
+export default handler
+global.tiktokvalle = [
+"https://files.catbox.moe/qp0xx5.mp4",
+"https://files.catbox.moe/bs834q.mp4",
+  "https://files.catbox.moe/7flsvo.mp4",
+  "https://files.catbox.moe/7flsvo.mp4",
+  "https://files.catbox.moe/l3lgtn.mp4",
+  "https://files.catbox.moe/dnq649.mp4",
+  "https://files.catbox.moe/0hm5e6.mp4",
+  "https://files.catbox.moe/d6ysm2.mp4",
+  "https://files.catbox.moe/0m3zlk.mp4",
+  "https://files.catbox.moe/9wk5jm.mp4",
+  "https://files.catbox.moe/3vypgu.mp4",
+  "https://files.catbox.moe/vo9hdh.mp4",
+  "https://files.catbox.moe/923uiu.mp4",
+  "https://files.catbox.moe/qvtspf.mp4",
+  "https://files.catbox.moe/embd2t.mp4",
+  "https://files.catbox.moe/3at7m3.mp4",
+  "https://files.catbox.moe/85v06n.mp4",
+"https://files.catbox.moe/6bp9ls.mp4",
+"https://files.catbox.moe/148602.mp4",
+  "https://files.catbox.moe/ij2ix1.mp4",
+  "https://files.catbox.moe/raqt0n.mp4",
+  "https://files.catbox.moe/u2w420.mp4",
+"https://files.catbox.moe/3qh4j7.mp4"
+]
